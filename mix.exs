9ea981b6698ae8007d8084bd89663c5cf841d481
Manh Van Vu <vuvanmanh123@gmail.com>
2026-07-27 13:19:54 +0700

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ClusterHelper.MixProject do
   def project do
     [
       app: :cluster_helper,
-      version: "1.0.0",
+      version: "1.0.1",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
